f8ffb787c117213d0a71507b009e456d3b4f451b
Divya-Manivannan1 <reethudya@gmail.com>
2024-03-25 12:49:46 +0000

    name search works

--- ticket-counter/src/App.tsx
+++ ticket-counter/src/App.tsx
@@ -1,4 +1,3 @@
-import { team } from "./data/team";
 import "./App.scss";
 import { EmployeeTileContainer } from "./components/EmployeeTileContainer/EmployeeTileContainer";
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <h1>Ticket Counter</h1>
-      <EmployeeTileContainer employes={team} />
+      <EmployeeTileContainer/>
     </div>
   );
 }
--- ticket-counter/src/components/EmployeeTileContainer/EmployeeTileContainer.tsx
+++ ticket-counter/src/components/EmployeeTileContainer/EmployeeTileContainer.tsx
@@ -1,30 +1,31 @@
+import { team } from "../../data/team.ts";
 import { Employee } from "../../types/teamType";
+import { FormEvent, useState } from "react";
 import { EmployeeTile } from "../EmployeeTile/EmployeeTile";
 import "./EmployeeTileContainer.scss";
 
-type EmployeeTileContainerProps = {
-  employes: Employee[];
-};
+export const EmployeeTileContainer = () => {
+  const [searchInput, setSearchInput] = useState<string>("");
 
-export const EmployeeTileContainer = ({
-  employes,
-}: EmployeeTileContainerProps) => {
-  let filteredEmployees: Employee[] = employes;
-  const handleNameInput = (event: Event) => {
-    filteredEmployees = filteredEmployees.filter((emp) =>
-      emp.name.includes(value)
-    );
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    const input = event.currentTarget.value.toLowerCase();
+    setSearchInput(input);
   };
+
+  const filteredEmployees: Employee[] = team.filter((emp) =>
+    emp.name.toLowerCase().includes(searchInput)
+  );
+
   return (
-    <div className="tile-container">
-      <input
-        type="text"
-        name="search by name"
-        onInput={handleNameInput}
-      ></input>
-      {filteredEmployees.map((emp) => {
-        return <EmployeeTile employee={emp} key={emp.id} />;
-      })}
+    <div className="tickets-display">
+      <div className="search-options">
+        <input type="text" name="search by name" onChange={handleInput}></input>
+      </div>
+      <div className="tile-container">
+        {filteredEmployees.map((emp) => {
+          return <EmployeeTile employee={emp} key={emp.id} />;
+        })}
+      </div>
     </div>
   );
 };
